ca89a35c58cbc93ad17d47eeeccee5eb16a56dd6
Emerson <emerson.day@gmail.com>
2020-10-13 16:32:03 -0700

    created cardFlip component and displayed in app component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import ReactCardFlip from "react-card-flip";
+import CardFlip from "./components/cardFlip/cardFlip";
 
 const App = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
-  return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-      <div>
-        This is the front of the card.
-        <button onClick={handleClick}>Click to flip</button>
-      </div>
-
-      <div>
-        This is the back of the card.
-        <button onClick={handleClick}>Click to flip</button>
-      </div>
-    </ReactCardFlip>
-  );
+  return <CardFlip />;
 };
 
 export default App;
