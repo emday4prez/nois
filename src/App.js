e2176daac18886926a1e978956c402dd06178672
Emerson <emerson.day@gmail.com>
2020-10-13 14:16:02 -0700

    adding files to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactCardFlip from "react-card-flip";
 
-function App() {
+const App = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <div>
+        This is the front of the card.
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+
+      <div>
+        This is the back of the card.
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+    </ReactCardFlip>
   );
-}
+};
 
 export default App;
